fe4959aa5752579c6dfc65f24b5cc72bbf99775b
frontEndDev <manish-resiliencesoft@gmail.com>
2023-04-13 10:55:50 +0530

    added comments

--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -3,5 +3,6 @@ import {FETCH_INVESTMENT_PRODUCT} from '../scence/investmentProduct/redux/Invest
 import {investmentSaga} from '../scence/investmentProduct/redux/InvestmentSaga';
 
 export default function* sagas() {
-  yield all([takeLatest(FETCH_INVESTMENT_PRODUCT, investmentSaga)]);
+  // yield all([takeLatest(FETCH_INVESTMENT_PRODUCT, investmentSaga)]);
+  //will be linking actions and sagas here which will be responsible for API calling..
 }
